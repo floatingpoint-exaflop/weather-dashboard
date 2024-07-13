db75236bbb25e87f520c4fc32413fb61b1817183
Frederico Swarthings <timscallon1@gmail.com>
2024-07-12 22:13:18 -0500

    began earnest work on files including js and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// ? Grab references to the important DOM elements.
+// ? Grab references to the important DOM elements to link to html.
 const timeDisplayEl = $('#time-display');
 const projectDisplayEl = $('#project-display');
 const projectFormEl = $('#project-form');
@@ -6,86 +6,60 @@ const projectNameInputEl = $('#project-name-input');
 const projectTypeInputEl = $('#project-type-input');
 const projectDateInputEl = $('#taskDueDate');
 
-// ? Helper function that displays the time, this is called every second in the setInterval function below.
+// ? Called every second in the setInterval function below.
 function displayTime() {
   const rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
 }
 
-// ? Reads projects from local storage and returns array of project objects.
-// ? If there are no projects in localStorage, it initializes an empty array ([]) and returns it.
-function readProjectsFromStorage() {
-  // ? Retrieve projects from localStorage and parse the JSON to an array.
-  // ? We use `let` here because there is a chance that there are no projects in localStorage (which means the projects variable will be equal to `null`) and we will need it to be initialized to an empty array.
-  let projects = JSON.parse(localStorage.getItem('projects'));
-
-  // ? If no projects were retrieved from localStorage, assign projects to a new empty array to push to later.
-  if (!projects) {
-    projects = [];
+// ? Reads forecasts from localStorage and return/parse the JSON to an array of forecast objects.
+// ? If there are no forecasts in localStorage, it initializes an empty array ([]) and returns it.
+function readForecastsFromStorage() {
+  let forecasts = JSON.parse(localStorage.getItem('forecasts'));
+  // ? If no forecasts in localStorage, make a new empty array.
+  if (!forecasts) {
+    forecasts = [];
   }
-
-  // ? Return the projects array either empty or with data in it whichever it was determined to be by the logic right above.
-  return projects;
+  // ? Return the forecasts array either empty or with data in it
+  return forecasts;
 }
 
-// ? Accepts an array of projects, stringifys them, and saves them in localStorage.
-function saveProjectsToStorage(projects) {
-  localStorage.setItem('projects', JSON.stringify(projects));
+
+
+// ? Accepts & stringifys array of forecasts & saves tolocalStorage.
+function saveForecastsToStorage(forecasts) {
+  localStorage.setItem('forecasts', JSON.stringify(forecasts));
 }
+
+
 
 // ? Creates a project card from the information passed in `project` parameter and returns it.
-function createProjectCard(project) {
-  const taskCard = $('<div>')
-    .addClass('card project-card draggable my-3')
-    .attr('data-project-id', project.id);
-  const cardHeader = $('<div>').addClass('card-header h4').text(project.name);
+function createForecastButton(forecast) {
+  const forcastCard = $('<div>')
+    .addClass('card project-card my-3');
+  const cardHeader = $('<div>').addClass('card-header h4').text(forecast.name);
   const cardBody = $('<div>').addClass('card-body');
-  const cardDescription = $('<p>').addClass('card-text').text(project.type);
-  const cardDueDate = $('<p>').addClass('card-text').text(project.dueDate);
+
   const cardDeleteBtn = $('<button>')
     .addClass('btn btn-danger delete')
-    .text('Delete')
-    .attr('data-project-id', project.id);
-  cardDeleteBtn.on('click', handleDeleteProject);
-
-  // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
-  if (project.dueDate && project.status !== 'done') {
-    const now = dayjs();
-    const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
-
-    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
-    if (now.isSame(taskDueDate, 'day')) {
-      taskCard.addClass('bg-warning text-white');
-    } else if (now.isAfter(taskDueDate)) {
-      taskCard.addClass('bg-danger text-white');
-      cardDeleteBtn.addClass('border-light');
-    }
-  }
-
+    .text('Delete');
+  cardDeleteBtn.on('click', handleDeleteForecast);
   // ? Gather all the elements created above and append them to the correct elements.
-  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
-  taskCard.append(cardHeader, cardBody);
+  cardBody.append(cardDescription, cardDeleteBtn);
+  forecastCard.append(cardHeader, cardBody);
 
   // ? Return the card so it can be appended to the correct lane.
   return taskCard;
 }
 
-function printProjectData() {
-  const projects = readProjectsFromStorage();
-
+function printForecastData() {
+  const forecasts = readForecastsFromStorage();
   // ? Empty existing project cards out of the lanes
-  const todoList = $('#todo-cards');
-  todoList.empty();
-
-  const inProgressList = $('#in-progress-cards');
-  inProgressList.empty();
-
-  const doneList = $('#done-cards');
-  doneList.empty();
-
-  // ? Loop through projects and create project cards for each status
-  for (let project of projects) {
-    if (project.status === 'to-do') {
+  const forecastList = $('#todo-cards');
+  forecastList.empty();
+  // ? Loop through forecasts and create forecast cards for each day
+  for (let forecast of forecasts) {
+    if (forecast.status === 'to-do') {
       todoList.append(createProjectCard(project));
     } else if (project.status === 'in-progress') {
       inProgressList.append(createProjectCard(project));
@@ -93,98 +67,36 @@ function printProjectData() {
       doneList.append(createProjectCard(project));
     }
   }
-
-  // ? Use JQuery UI to make task cards draggable
-  $('.draggable').draggable({
-    opacity: 0.7,
-    zIndex: 100,
-    // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
-    helper: function (e) {
-      // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
-      const original = $(e.target).hasClass('ui-draggable')
-        ? $(e.target)
-        : $(e.target).closest('.ui-draggable');
-      // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
-      return original.clone().css({
-        width: original.outerWidth(),
-      });
-    },
-  });
-}
-
-// ? Removes a project from local storage and prints the project data back to the page
-function handleDeleteProject() {
-  const projectId = $(this).attr('data-project-id');
-  const projects = readProjectsFromStorage();
-
-  // ? Remove project from the array. There is a method called `filter()` for this that is better suited which we will go over in a later activity. For now, we will use a `forEach()` loop to remove the project.
-  projects.forEach((project) => {
-    if (project.id === projectId) {
-      projects.splice(projects.indexOf(project), 1);
+  // ? Remove project from the array with a `forEach()` loop to remove the project.
+  forecasts.forEach((forecast) => {
+    if (forecast.id === forecastId) {
+      forecasts.splice(forecasts.indexOf(forecast), 1);
     }
   });
-
-  // ? We will use our helper function to save the projects to localStorage
-  saveProjectsToStorage(projects);
-
-  // ? Here we use our other function to print projects back to the screen
-  printProjectData();
+  // ? We will use our helper function to save the forecasts to localStorage
+  saveforecastsToStorage(forecasts);
+  // ? Here we use our other function to print forecasts back to the screen
+  printForecastData();
 }
 
 // ? Adds a project to local storage and prints the project data
 function handleProjectFormSubmit(event) {
   event.preventDefault();
-
   // ? Read user input from the form
   const projectName = projectNameInputEl.val().trim();
   const projectType = projectTypeInputEl.val(); // don't need to trim select input
   const projectDate = projectDateInputEl.val(); // yyyy-mm-dd format
-
-  const newProject = {
-    // ? Here we use a Web API called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
-    id: crypto.randomUUID(),
-    name: projectName,
-    type: projectType,
-    dueDate: projectDate,
-    status: 'to-do',
-  };
-
-  // ? Pull the projects from localStorage and push the new project to the array
-  const projects = readProjectsFromStorage();
-  projects.push(newProject);
-
-  // ? Save the updated projects array to localStorage
-  saveProjectsToStorage(projects);
-
+  // ? Pull the forecasts from localStorage and push the new project to the array
+  const forecasts = readForecastsFromStorage();
+  forecasts.push(newProject);
+  // ? Save the updated forecasts array to localStorage
+  saveforecastsToStorage(forecasts);
   // ? Print project data back to the screen
   printProjectData();
-
   // ? Clear the form inputs
   projectNameInputEl.val('');
   projectTypeInputEl.val('');
   projectDateInputEl.val('');
-}
-
-// ? This function is called when a card is dropped into a lane. It updates the status of the project and saves it to localStorage. You can see this function is called in the `droppable` method below.
-function handleDrop(event, ui) {
-  // ? Read projects from localStorage
-  const projects = readProjectsFromStorage();
-
-  // ? Get the project id from the event
-  const taskId = ui.draggable[0].dataset.projectId;
-
-  // ? Get the id of the lane that the card was dropped into
-  const newStatus = event.target.id;
-
-  for (let project of projects) {
-    // ? Find the project card by the `id` and update the project status.
-    if (project.id === taskId) {
-      project.status = newStatus;
-    }
-  }
-  // ? Save the updated projects array to localStorage (overwritting the previous one) and render the new project data to the screen.
-  localStorage.setItem('projects', JSON.stringify(projects));
-  printProjectData();
 }
 
 // ? Add event listener to the form element, listen for a submit event, and call the `handleProjectFormSubmit` function.
@@ -206,11 +118,5 @@ $(document).ready(function () {
   $('#taskDueDate').datepicker({
     changeMonth: true,
     changeYear: true,
-  });
-
-  // ? Make lanes droppable
-  $('.lane').droppable({
-    accept: '.draggable',
-    drop: handleDrop,
   });
 });
